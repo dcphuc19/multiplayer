65e8a185ac870ea7c195df78fa78592df7a17bc0
phuc <phucforwork99@gmail.com>
2026-03-05 22:29:41 +0700

    update rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 3.5f;
+    public float moveSpeed = 3.5f;
+    public float rotateSpeed = 100.0f;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -13,8 +14,15 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical") + transform.right * speed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 rotateY = new Vector3(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
+        if (movement != Vector3.zero)
+        {
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
+        }
+        rb.MovePosition(rb.position + transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime +
+        transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
     }
 }
